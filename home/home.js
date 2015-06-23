7e1ba732276b617577bb942e7dcb26ee7f7d2093
victorhqc <victorhqc@gmail.com>
2015-06-23 14:03:35 -0600

    Fixes y servicios

--- home/home.js
+++ home/home.js
@@ -3,6 +3,7 @@ angular.module('home', ['ui.bootstrap','ui.utils','ngRoute','ngAnimate']);
 angular.module('home').config(function($routeProvider) {
 
     $routeProvider.when('/home',{templateUrl: 'home/partial/home/home.html'});
+    $routeProvider.when('/services',{templateUrl: 'home/partial/services/services.html'});
     /* Add New Routes Above */
 
 });
---
+++ home/partial/services/services.js
@@ -0,0 +1,4 @@
+angular.module('home').controller('ServicesCtrl',function($scope){
+
+
+});
